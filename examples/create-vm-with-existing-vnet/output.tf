2176f90c8357c2f50d740a1157b362cad02602da
rohit <rohittiwari612@gmail.com>
2023-10-04 16:05:09 +0530

    Added Output

--- examples/create-vm-with-existing-vnet/output.tf
+++ examples/create-vm-with-existing-vnet/output.tf
@@ -21,3 +21,11 @@ output "pan_node_name" {
 # output "psn_node_detail" {
 #   value = join(":", [module.ise_psn_vm_cluster.node_name, module.ise_psn_vm_cluster.private_address])
 # }
+
+output "private_dns_records" {
+  value = module.loadbalancer_dns.private_dns_records
+}
+
+output "loadbalancer_frontendIP" {
+  value = module.loadbalancer_dns.loadbalancer_frontendIP
+}
