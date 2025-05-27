adbcc422e370d07d8cd98e4c963d0ad03ff72774
rohit <rohitti@cisco.com>
2025-05-27 11:23:49 +0530

    Added changes for ISE release 3.4

--- module/loadbalancer_dns/functionapp.tf
+++ module/loadbalancer_dns/functionapp.tf
@@ -28,6 +28,7 @@ resource "azurerm_storage_account" "ise-app-storage" {
   account_tier                  = "Standard"
   account_replication_type      = "GRS"
   public_network_access_enabled = false
+  allow_nested_items_to_be_public = false
 }
 
 resource "random_string" "function_app_suffix" {
